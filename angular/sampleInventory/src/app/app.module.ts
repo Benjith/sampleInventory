61d37feacbf4f0e90c2bc963a1338677745064a9
cmuhsin <>
2018-09-17 12:52:22 +0530

    firstCommit
    signUp singIn

--- angular/sampleInventory/src/app/app.module.ts
+++ angular/sampleInventory/src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { routes } from './/app-routing.module'
+import { RouterModule } from '@angular/router';
+import { SigninComponent } from './mainhome/signin/signin.component';
+import { SignupComponent } from './mainhome/signup/signup.component';
+import { HomeComponent } from './mainhome/home/home.component';
+import { DashboardComponent } from './poloAcc/dashboard/dashboard.component';
+import {FormsModule} from '@angular/forms'
+import {HttpModule} from '@angular/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
